85d02218759bffbac509736f900c2169383566f5
Muhamad Lutfi <lutfi787878@gmail.com>
2023-09-14 10:25:09 +0700

    perubahan controller.php

--- controller.php
+++ controller.php
@@ -5,6 +5,15 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        intl_get_error_message
+    </body>
+    </html>
 </body>
 </html>
